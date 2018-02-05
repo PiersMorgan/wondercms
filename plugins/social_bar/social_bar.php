e226be46f32ecb831d4c53545d921803336f3f97
JL <jl@jakee.pw>
2018-02-05 10:51:40 +0000

    Plugin Implementation
    Working on plugin implementation to replace theme implentation i made.

---
+++ plugins/social_bar/social_bar.php
@@ -0,0 +1,38 @@
+<?php
+if(defined('VERSION'))
+	define('version', VERSION);
+	defined('version') OR die('Direct access is not allowed.');
+
+	 wCMS::addListener('js', 'loadAdditionContentsJS');
+	 wCMS::addListener('css', 'loadAdditionContentsCSS');
+	 wCMS::addListener('page', 'loadAdditionContentsEditableV2');
+/*
+function loadAdditionContentsJS($args) {
+	$script = <<<'EOT'
+
+<script src=""></script>
+EOT;
+	if(version<'2.0.0')
+		array_push($args[0], $script);
+	else
+		$args[0].=$script;
+	return $args;
+}
+*/
+function loadAdditionContentsCSS($args) {
+	$script = <<<'EOT'
+<link rel="stylesheet" href="plugins/social_bar/bar.min.css" type="text/css" media="screen" charset="utf-8">
+EOT;
+	if(version<'2.0.0')
+		array_push($args[0], $script);
+	else
+		$args[0].=$script;
+	return $args;
+}
+ 
+ 
+ 
+ // code here
+
+
+?>
